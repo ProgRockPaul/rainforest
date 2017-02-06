cff5273b496b369d6fd873347d4b92b1595ff33a
Paul Walsh <twentytwentyk@live.com>
2017-02-06 16:48:17 -0500

    Writing product validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
   resources :products
+  #
+  # get '/products', to: 'products#index'
+  # get '/products/new', to: 'products#new'
+  # get '/products/:id', to: 'products#show',
+  # get '/products/:id/edit', to: 'products#edit', as: "edit"
+  # post '/products', to: 'products#create'
+  # post '/products/:id', to: 'products#update'
+  # delete '/products/:id', to: 'products#destroy'
+
+# get /products, to controller products action index
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
