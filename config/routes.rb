a61213ea9582d42d6c88c797d09ecb0c31c38c9d
Paul Walsh <twentytwentyk@live.com>
2017-02-06 17:33:25 -0500

    Create login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :products
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   #
   # get '/products', to: 'products#index'
   # get '/products/new', to: 'products#new'
